540c87f32df7f59c1585ccdb9bf6b8ac948e701d
WLan1707 <muhammadwlan@gmail.com>
2025-10-26 23:39:23 +0700

    Tipe Data Dasar dan HandCount

---
+++ src/hand.hs
@@ -0,0 +1,43 @@
+import qualified Data.Map.Strict as Map
+
+-- Membuat ADT untuk jenis tile
+data Suit = Manzu | Pinzu | Souzu | Honor
+    deriving (Show, Eq, Ord, Enum, Bounded)
+
+data Tile = Tile Suit Int
+    deriving (Show, Eq)
+
+instance Ord Tile where
+    compare :: Tile -> Tile -> Ordering
+    compare (Tile s1 n1) (Tile s2 n2)
+        | s1 == s2   = compare n1 n2
+        | otherwise  = compare s1 s2
+
+data Meld 
+    = Sequence Tile         -- Sequence M4 artinya M4, M5, M6
+    | Triplet Tile          -- Triplet Z3 artinya Z3, Z3, Z3
+
+data PMeld 
+    = MissingMiddle Tile    -- MissingMiddle P4 artinya ada P4 dan P6
+    | MissingOut Tile       -- MissingOut S2 artinya ada S2 dan S3
+
+newtype Pair = Pair Tile  -- Pair P3 artinya P3, P3
+
+type Hand = [Tile]
+
+-- Tangan kemenangan
+data AgariHand 
+    = Standard [Meld] Pair  -- 4 Meld + 1 Pair 
+    | Chiitoi [Pair]        -- 7 Pair
+    | Kokushi Hand          -- 13 + 1 Honor & Terminal
+
+type HandCount = Map.Map Tile Int
+
+handToCount :: Hand -> HandCount
+handToCount hand = Map.fromListWith (+) [(tile, 1) | tile <- hand]
+
+findPair :: HandCount -> [Pair]
+findPair = map Pair . Map.keys . Map.filter (>= 2)
+
+findTriplet :: HandCount -> [Meld]
+findTriplet = map Triplet . Map.keys . Map.filter (>= 3)
